488a4c40d186c07ca63a1070a0185413a0332892
EL3GOST <langot.benjamin@gmail.com>
2018-09-21 23:07:43 +0200

    Premier commit

---
+++ Assets/Scripts/DisplayText.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplayText : MonoBehaviour {
+
+    public string texte = "J'ai un gros zizi bien poilu";
+    public float vitesseDefilement = 1f;
+
+    private int avancementDefilement;
+    private TextMesh zoneTexte;
+
+
+    // Use this for initialization
+    void Start () {
+        avancementDefilement = 0;
+        zoneTexte = GetComponent<TextMesh>();
+        StartCoroutine(ExecuteApresDelai(1));
+    }
+	
+	// Update is called once per frame
+	/*void Update () {
+        if(avancementDefilement < texte.Length)
+            zoneTexte.color.a
+        zoneTexte.text += gererDefilement();
+
+	}*/
+
+    void AffichageLettre(){
+
+    }
+
+    string gererDefilement()
+    {
+        if (avancementDefilement < texte.Length)
+        {
+            //TROUVER UN MOYEN DE FAIRE DEFILER TEXTE
+            return texte[avancementDefilement++].ToString();
+        }
+        return "";
+    }
+
+    IEnumerator ExecuteApresDelai(float delai){
+        while (true) { 
+        yield return new WaitForSeconds(delai);
+            if (avancementDefilement < texte.Length)
+            {
+                zoneTexte.text += gererDefilement();
+            }
+        }
+    }
+}
